9d38f49f95887ae9eff3d120144b46b6c12e14fa
Mohammad Ayaan Siddiqui <ayaangames@gmail.com>
2022-08-07 08:17:08 +0530

    added assets and created boilerplate for all pages and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetails from "./pages/ExerciseDetails";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box width={"400px"} sx={{ width: { xl: "1448px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/exercise/:id" element={<ExerciseDetails />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
